b81e51d019bfcc87fd9e97da808c20c6c60074d3
1325431048-au52 <saurabhdixit5058@gmail.com>
2023-12-07 23:58:24 +0530

    my finalcode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
 // Import area
 
 //function area
-  const name = "Saurabh Dixit" // this is a global variable
-  // a variable define outside the fuction then it will be a global variable
-  function Home() {
-    // Whenever we define a varable inside a fuction body then it will be local variable;
-    const mission = "OKlabs" // this is a loacal varibale
+const name = 'Saurabh Dixit' // this is a global variable
+// a variable define outside the fuction then it will be a global variable
+function Home() {
+  // Whenever we define a varable inside a fuction body then it will be local variable;
+  const mission = 'OKlabs' // this is a loacal varibale
   return (
     <main>
-      <h1>Hello{name} {mission}</h1>
+      <h1>
+        Hello {name} {mission}
+      </h1>
     </main>
   )
 }
 //export area
-export default Home;
+export default Home
